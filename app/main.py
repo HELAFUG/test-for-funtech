8cdad433571ef7d1d2c0e398dcc1229327f00cc1
HELAFUG <loxand42@gmail.com>
2025-12-17 22:58:12 +0300

    add main file

---
+++ app/main.py
@@ -0,0 +1,30 @@
+import logging
+import uvicorn
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from core.config import settings
+from core.helpers import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.basicConfig(
+        level=settings.log.level,
+        format=settings.log.format,
+        datefmt=settings.log.datefmt,
+    )
+    yield
+
+    await db_helper.close_db_engine()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
