920bfe0c1683f9ded62db372c5c406077d21be2c
HELAFUG <loxand42@gmail.com>
2025-12-17 22:41:34 +0300

    add database naming convention

--- app/core/config.py
+++ app/core/config.py
@@ -13,6 +13,13 @@ class DBConfig(BaseModel):
         "DB_URL", "postgresql://postgres:postgres@localhost:5434/postgres"
     )
     echo: bool = False
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class LogConfig(BaseModel):
