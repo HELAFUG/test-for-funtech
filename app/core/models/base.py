4671d472ed1a782dca8e017dcd39ce7153bc69a6
HELAFUG <loxand42@gmail.com>
2025-12-17 22:44:36 +0300

    add base model

---
+++ app/core/models/base.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, declared_attr
+from sqlalchemy import MetaData
+from core.config import settings
+from utils.case_converter import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+
+    __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
+
+    @declared_attr
+    def __tablename__(cls):
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
