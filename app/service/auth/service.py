cda53e5fc6ce5fcf4b36972d115f25ae30140f97
HELAFUG <loxand42@gmail.com>
2025-12-18 15:35:08 +0300

    i forgot a decrypt password

--- app/service/auth/service.py
+++ app/service/auth/service.py
@@ -1,10 +1,11 @@
 from typing import Optional
+from datetime import datetime
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.schemas.user import UserCreate, UserRead
 from core.schemas.access_token import AccessToken
 from core.models import User
 from repository.auth import create_user, get_user
-from service.security.password import hash_password
+from service.security.password import hash_password, verify_password
 from service.jwt.token import encode_token
 
 
@@ -14,10 +15,20 @@ async def new_user(user: UserCreate, session: AsyncSession) -> Optional[UserRead
     return await create_user(session=session, user=user_db)
 
 
-async def get_exist_user(username: str, session: AsyncSession) -> Optional[AccessToken]:
+async def get_exist_user(
+    username: str, password: str, session: AsyncSession
+) -> Optional[AccessToken]:
     user = await get_user(session=session, username=username)
-    if user:
-        token = encode_token(payload={"sub": user.username})
+    decrypt_result = verify_password(password=password, hashed_password=user.password)
+
+    if user and decrypt_result:
+        token = encode_token(
+            payload={
+                "sub": user.username,
+                "exp": datetime.utcnow() + datetime.timedelta(minutes=30),
+                "iat": datetime.utcnow(),
+            }
+        )
         return AccessToken(access_token=token, token_type="bearer")
 
     return None
