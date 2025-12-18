1e73c8325f6a9ee4f73d887bd9015388b0c65afb
HELAFUG <loxand42@gmail.com>
2025-12-18 15:58:27 +0300

    oh man it  is work

--- app/api/auth/dependencies.py
+++ app/api/auth/dependencies.py
@@ -2,12 +2,11 @@ from typing import Annotated
 from fastapi.security.oauth2 import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from jose import JWTError
 from service.jwt.token import decode_token
 from repository.auth import get_user
 from core.helpers import db_helper
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/token")
 
 
 async def get_current_user(
@@ -20,13 +19,11 @@ async def get_current_user(
         headers={"WWW-Authenticate": "Bearer"},
     )
 
-    try:
-        payload = decode_token(token)
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except JWTError:
+    payload = decode_token(token)
+    username: str = payload.get("sub")
+    if username is None:
         raise credentials_exception
+
     user = await get_user(username=username, session=session)
     if user is None:
         raise credentials_exception
--- app/api/auth/router.py
+++ app/api/auth/router.py
@@ -1,9 +1,12 @@
 from typing import Optional, Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, HTTPException, Depends
-from service.auth import new_user
+from fastapi.security import OAuth2PasswordRequestForm
+from service.auth import new_user, get_exist_user
 from core.schemas.user import UserCreate, UserRead
+from core.schemas.access_token import AccessToken
 from core.helpers import db_helper
+from api.auth.dependencies import get_current_user
 
 router = APIRouter()
 
@@ -18,3 +21,25 @@ async def register_new_user(
         return user
 
     raise HTTPException(status_code=400, detail="User already exists")
+
+
+@router.post("/token", response_model=AccessToken)
+async def login_exist_user(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    token = await get_exist_user(
+        session=session,
+        username=form_data.username,
+        password=form_data.password,
+    )
+
+    if token:
+        return token
+
+    raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+
+@router.get("/me")
+async def read_users_me(current_user: Annotated[UserRead, Depends(get_current_user)]):
+    return current_user
