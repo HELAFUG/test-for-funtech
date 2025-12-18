cfa142cbaed02ee5e334f8fe508c82dea57451fa
HELAFUG <loxand42@gmail.com>
2025-12-18 20:25:37 +0300

    fix bug with type annotation

--- app/core/models/mixins/id_uuid_pk.py
+++ app/core/models/mixins/id_uuid_pk.py
@@ -1,7 +1,6 @@
 import uuid
-from sqlalchemy import UUID
 from sqlalchemy.orm import mapped_column, Mapped
 
 
 class UUIDPkMixin:
-    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
